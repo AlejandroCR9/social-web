90c9cace8fc09cb85ccbbbf7f08e8878bab1a077
Alejandro Cedillo <aleejandro.99r@gmail.com>
2023-06-07 14:55:28 -0600

    add debug toolbar for dev

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -45,12 +45,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'debug_toolbar',
     'easy_thumbnails',
     'images.apps.ImagesConfig',
     'social_django',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -185,3 +187,7 @@ ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail',
                                         args=[u.username])
 }
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
